6426ee482a5ad06ef9c7b39d86bd96ad3f5bae7a
chadnelson101 <chadnelson279@gmail.com>
2024-02-27 15:42:12 +0200

    added

--- middlewear/authenticate.js
+++ middlewear/authenticate.js
@@ -6,7 +6,7 @@ export default{
         // Retrieve hashed password from the database based on the username (firstName)
         const hashedPassword = await loginUser(FirstName);
         // Compare the provided password with the hashed password
-        bcrypt.compare(userPass, hashedPassword, (err, result) => {
+        bcrypt.compare(userPass, hashedPassword, (err, result) => {https://github.com/chadnelson101/backend.git
             if (err) {
                 throw err;
             }
